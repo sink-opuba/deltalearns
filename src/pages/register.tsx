8c0c9b885088ba1c178360781425eb535cc239a4
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-05-07 01:22:06 +0100

    add registe route

---
+++ src/pages/register.tsx
@@ -0,0 +1,15 @@
+// Gatsby supports TypeScript natively!
+import React from "react"
+import { PageProps, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const RegisterPage = (props: PageProps) => (
+  <Layout>
+    <SEO title="Register" />
+    <h1>COMING SOON!!!</h1>
+  </Layout>
+)
+
+export default RegisterPage
