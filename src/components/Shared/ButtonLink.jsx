9a32cbac3080d52edd11177807ce89d064faa9ae
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-05-21 19:10:13 +0100

    round out Buttonlink component logic

--- src/components/Shared/ButtonLink.jsx
+++ src/components/Shared/ButtonLink.jsx
@@ -1,17 +1,32 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Link } from "gatsby"
 
-import "./ButtonLink.scss"
+// import "./ButtonLink.scss"
 
-const ButtonLink = ({ bgColor, color, dest, text }) => {
-  return <Link to=""></Link>
+const ButtonLink = ({ bgColor, color, dest, children }) => {
+  return (
+    <Link
+      style={{ backgroundColor: bgColor, color }}
+      className="btn-link"
+      to={`${dest}`}
+    >
+      {children}
+    </Link>
+  )
 }
 
 export default ButtonLink
 
+ButtonLink.defaultProps = {
+  bgColor: "#4db0e7",
+  color: "#ffffff",
+  dest: "/",
+}
+
 ButtonLink.propTypes = {
-  bgColor: PropTypes.string.isRequired,
-  color: PropTypes.string.isRequired,
+  bgColor: PropTypes.string,
+  color: PropTypes.string,
   dest: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
 }
