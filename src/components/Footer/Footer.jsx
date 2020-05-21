4910eaa4d31a441ab1d3cfc4b6b9bfc500015c4e
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-05-21 12:30:11 +0100

    adjust footer icon size

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -49,7 +49,7 @@ const Footer = () => {
                   href="https://www.google.com/maps/place/Warri+Innovation+Hub/@5.5300747,5.7717313,17z/data=!4m25!1m19!4m18!1m9!2m2!1d5.7826606!2d5.5440522!3m4!1m2!1d5.7752192!2d5.529821!3s0x1041b27ef317b31f:0x5a79579a6b98173b!4e1!1m6!1m2!1s0x1041b36023a3e9ad:0x8431eccdd7bd5ab7!2swarri+innovation+hub!2m2!1d5.7720592!2d5.5272815!3e0!3m4!1s0x1041b36023a3e9ad:0x8431eccdd7bd5ab7!8m2!3d5.5272815!4d5.7720592"
                 >
                   <span>
-                    <MdLocationOn />
+                    <MdLocationOn size={19} />
                   </span>
                   3rd floor, Isoken Plaza, Enerhen Junction, Warri.
                 </a>
@@ -101,7 +101,7 @@ const Footer = () => {
                     required
                   />
                   <button type="submit">
-                    <MdArrowForward />
+                    <MdArrowForward size={25} />
                   </button>
                 </div>
               </form>
